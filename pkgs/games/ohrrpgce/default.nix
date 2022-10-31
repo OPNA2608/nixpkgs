3075351934418c971f6441dd84b793bcc6e773fa
OPNA2608 <christoph.neidahl@gmail.com>
2022-10-31 10:51:32 +0100

    ohrrpgce.unstable: 13037 -> 13141

--- pkgs/games/ohrrpgce/default.nix
+++ pkgs/games/ohrrpgce/default.nix
@@ -7,17 +7,17 @@ let
 in
 rec {
   # Broken
-  hrodvitnir = common {
-    codename = "hrodvitnir";
-    isRelease = true;
-    rev = "12432";
-    sha256 = "sha256-8Dgekeu2j2QlfrhOyq4PilT450PoO31tu9Kh8W5d+lo=";
-  };
-  stable = hrodvitnir;
+  # hrodvitnir = common {
+  #   codename = "hrodvitnir";
+  #   isRelease = true;
+  #   rev = "12432";
+  #   sha256 = "sha256-8Dgekeu2j2QlfrhOyq4PilT450PoO31tu9Kh8W5d+lo=";
+  # };
+  # stable = hrodvitnir;
   # Working
   unstable = common {
     codename = "ichorescent";
-    rev = "13037";
-    sha256 = "sha256-AgaXQtGKo8Zajbe9hG4dOGywortF0XN/5fdl/MayyZ8=";
+    rev = "13141";
+    sha256 = "sha256-5arpekJRSf+5Hgqhs5CNghkAYPyFXXv94D/kouiCZcU=";
   };
 }
