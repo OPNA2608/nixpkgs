57f85659a6735f403a91b593b2b0780286e27874
OPNA2608 <christoph.neidahl@gmail.com>
2023-01-23 22:12:28 +0100

    libusermetrics: meta, tests

--- pkgs/development/libraries/libusermetrics/default.nix
+++ pkgs/development/libraries/libusermetrics/default.nix
@@ -29,6 +29,9 @@ stdenv.mkDerivation rec {
   postPatch = ''
     substituteInPlace data/CMakeLists.txt \
       --replace '/etc' "$out/etc"
+  '' + lib.optionalString (!doCheck) ''
+    # Only needed by tests
+    sed -i -e '/QTDBUSTEST/d' CMakeLists.txt
   '';
 
   strictDeps = true;
@@ -43,17 +46,36 @@ stdenv.mkDerivation rec {
   buildInputs = [
     cmake-extras
     gsettings-qt
-    gtest
     libapparmor
-    libqtdbustest
     qdjango
     qtbase
     qtxmlpatterns
+  ];
+
+  checkInputs = [
+    gtest
+    libqtdbustest
   ];
 
   dontWrapQtApps = true;
 
   cmakeFlags = [
     "-DGSETTINGS_LOCALINSTALL=ON"
+    "-DENABLE_TESTS=${lib.boolToString doCheck}"
   ];
+
+  # Tests rely on system D-Bus, flaky
+  doCheck = false;
+
+  preCheck = ''
+    export QT_PLUGIN_PATH=${lib.getBin qtbase}/lib/qt-${qtbase.version}/plugins/
+  '';
+
+  meta = with lib; {
+    description = "Enables apps to locally store interesting numerical data for later presentation";
+    homepage = "https://gitlab.com/ubports/development/core/libusermetrics";
+    license = licenses.lgpl3Only;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ OPNA2608 ];
+  };
 }
