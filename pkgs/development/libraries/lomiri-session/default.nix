a89e68bb20c3b05b2caea80aed13ea5c5c17f057
OPNA2608 <christoph.neidahl@gmail.com>
2023-02-18 23:55:01 +0100

    lomiri-session: Pull in Debian patches
    See comments for what they address. The Lomiri desktop session launches now, but is still very broken.
    
    - many things cause it to crash
    - FF and many other applications dont appear
    - taking screenshots might not work?
    - indicators missing (libayatana-common, i think)
    - it is hard to exit without the UI elements to properly log out, particularly because it likes to restarts itself

--- pkgs/development/libraries/lomiri-session/default.nix
+++ pkgs/development/libraries/lomiri-session/default.nix
@@ -1,6 +1,7 @@
 { stdenvNoCC
 , lib
 , fetchFromGitLab
+, fetchpatch
 , cmake
 , dbus
 , deviceinfo
@@ -8,6 +9,7 @@
 , lomiri
 , makeWrapper
 , pkg-config
+, runtimeShell
 , systemd
 }:
 
@@ -21,6 +23,30 @@ stdenvNoCC.mkDerivation rec {
     rev = version;
     hash = "sha256-1ZpAn1tFtlXIfeejG0TnrJBRjf3tyz7CD+riWo+sd0s=";
   };
+
+  patches = [
+    # Removes broken first-time wizard check
+    (fetchpatch {
+      url = "https://salsa.debian.org/ubports-team/lomiri-session/-/raw/9522c7b23ab53aa270b26ed60b7b87a95995f453/debian/patches/0001_desktop-dm-lomiri-session-Drop-old-wizard-has-run-ch.patch";
+      hash = "sha256-AIwgztFOGwG2zUsaUen/Z3Mes9m7VgbvNKWp/qYp4g4=";
+    })
+    # Fix quoting on ps check
+    (fetchpatch {
+      url = "https://salsa.debian.org/ubports-team/lomiri-session/-/raw/58b8e4e8b8316cdacfde942b8288f792beb65cd5/debian/patches/0002_lomiri-session-Put-evaluation-of-ps-call-in-quotes.patch";
+      hash = "sha256-SwTIsB0zLRJupkikeVNrXSduaCNke/XWCWBgaNUkCtU=";
+    })
+    # Properly gate of UBtouch-specific code
+    # Otherwise session won't launch, errors out on a removed Mir setting
+    (fetchpatch {
+      url = "https://salsa.debian.org/ubports-team/lomiri-session/-/raw/58b8e4e8b8316cdacfde942b8288f792beb65cd5/debian/patches/0003_lomiri-session-Properly-differentiate-between-Ubuntu.patch";
+      hash = "sha256-eFiagFEpH43WpVGA6xkI1IiQ99HHizonhXYg1wYAhwU=";
+    })
+    # Remove outdated Before & Wants targets
+    (fetchpatch {
+      url = "https://salsa.debian.org/ubports-team/lomiri-session/-/raw/d2eb6ca69fca00baf02003b789edcfdfad4f5e61/debian/patches/0005_systemd-lomiri.service-Drop-Before-and-Wants-for-ind.patch";
+      hash = "sha256-vGFvcCjbwcuLrAUIsL5y/QmoOR5i0560LNv01ZT9OOg=";
+    })
+  ];
 
   postPatch = ''
     substituteInPlace CMakeLists.txt \
@@ -56,6 +82,8 @@ stdenvNoCC.mkDerivation rec {
   ];
 
   postInstall = ''
+    substituteInPlace $out/bin/lomiri-session \
+      --replace '/bin/bash' '${runtimeShell}'
     wrapProgram $out/bin/lomiri-session \
       --prefix PATH : ${lib.makeBinPath [ deviceinfo inotify-tools lomiri ]}
   '';
