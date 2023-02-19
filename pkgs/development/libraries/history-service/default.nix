01fca3b195e62f26447f30593d1dd8f1c1312563
OPNA2608 <christoph.neidahl@gmail.com>
2023-02-20 00:03:56 +0100

    history-service: Cleanup, add meta, work on enabling tests
    Tests still disabled, only 50% of them pass. :(

--- pkgs/development/libraries/history-service/default.nix
+++ pkgs/development/libraries/history-service/default.nix
@@ -3,17 +3,21 @@
 , fetchFromGitLab
 , fetchpatch
 , cmake
-, pkg-config
-, qtdeclarative
-, lomiri-api
-, qtbase
-, libqtdbustest
-, qtpim
+, dbus
+, dbus-test-runner
+, dconf
+, gnome
 , libphonenumber
+, libqtdbustest
+, lomiri-api
+, pkg-config
+, protobuf
+, qtbase
+, qtdeclarative
+, qtpim
 , telepathy
 , telepathy-mission-control
-, dconf
-, protobuf
+, xvfb-run
 }:
 
 stdenv.mkDerivation rec {
@@ -38,6 +42,8 @@ stdenv.mkDerivation rec {
   ];
 
   postPatch = ''
+    export VERBOSE=1
+
     # Uses pkg_get_variable, cannot substitute prefix with that
     substituteInPlace daemon/CMakeLists.txt \
       --replace 'DESTINATION ''${SYSTEMD_USER_UNIT_DIR}' 'DESTINATION "${placeholder "out"}/lib/systemd/user"'
@@ -50,7 +56,11 @@ stdenv.mkDerivation rec {
     substituteInPlace tests/common/dbus-services/CMakeLists.txt \
       --replace "\''${DBUS_SERVICES_DIR}/org.freedesktop.Telepathy.MissionControl5.service" "${telepathy-mission-control}/share/dbus-1/services/org.freedesktop.Telepathy.MissionControl5.service" \
       --replace "\''${DBUS_SERVICES_DIR}/org.freedesktop.Telepathy.AccountManager.service" "${telepathy-mission-control}/share/dbus-1/services/org.freedesktop.Telepathy.AccountManager.service" \
-      --replace "\''${DBUS_SERVICES_DIR}/ca.desrt.dconf.service" "${dconf}/share/dbus-1/services/ca.desrt.dconf.service
+      --replace "\''${DBUS_SERVICES_DIR}/ca.desrt.dconf.service" "${dconf}/share/dbus-1/services/ca.desrt.dconf.service"
+
+    substituteInPlace cmake/modules/GenerateTest.cmake \
+      --replace '/usr/lib/dconf' '${lib.getLib dconf}/libexec' \
+      --replace '/usr/lib/telepathy' '${lib.getLib telepathy-mission-control}/libexec'
   '' else ''
     sed -i -e '/add_subdirectory(tests)/d' CMakeLists.txt
   '');
@@ -63,14 +73,21 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    libphonenumber
+    protobuf
     qtbase
     qtdeclarative
     qtpim
-    libphonenumber
     telepathy
+  ];
 
-    # libphonenumber
-    protobuf
+  nativeCheckInputs = [
+    dbus
+    dbus-test-runner
+    dconf
+    gnome.gnome-keyring
+    telepathy-mission-control
+    xvfb-run
   ];
 
   dontWrapQtApps = true;
@@ -80,6 +97,35 @@ stdenv.mkDerivation rec {
     "-DENABLE_WERROR=OFF"
   ];
 
-  # TODO
+  # 50% of tests still fail
+  # doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
   doCheck = false;
+
+  # Multiple tests try to access the same sqlite db
+  enableParallelChecking = false;
+
+  checkPhase = ''
+    runHook preCheck
+
+    export QT_PLUGIN_PATH=${lib.getBin qtbase}/lib/qt-${qtbase.version}/plugins
+    export HOME=$PWD
+    dbus-run-session --config-file=${dbus}/share/dbus-1/session.conf -- make test
+
+    runHook postCheck
+  '';
+
+  meta = with lib; {
+    description = "Service that provides call log and conversation history";
+    longDescription = ''
+      History service provides the database and an API to store/retrieve the call log (used by dialer-app) and the sms/mms history (used by messaging-app).
+
+      See as well telepathy-ofono for incoming message events.
+
+      Database location: ~.local/share/history-service/history.sqlite
+    '';
+    homepage = "https://gitlab.com/ubports/development/core/history-service";
+    license = licenses.;
+    platforms = platforms.;
+    maintainers = with maintainers; [ OPNA2608 ];
+  };
 }
