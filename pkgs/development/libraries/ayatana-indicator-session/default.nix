1bc25ef65d83b126a6d9428e1c1843e03b04c084
OPNA2608 <christoph.neidahl@gmail.com>
2023-02-24 23:18:11 +0100

    ayatana-indicator-session: Apply patch for full Lomiri support

--- pkgs/development/libraries/ayatana-indicator-session/default.nix
+++ pkgs/development/libraries/ayatana-indicator-session/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, fetchpatch
 , cmake
 , cmake-extras
 , dbus
@@ -24,7 +25,22 @@ stdenv.mkDerivation rec {
     hash = "sha256-nQukI0ClG5DoallzMvODRPUZzar5rTBxwcAaEMvdVYo=";
   };
 
+  patches = [
+    # Full Lomiri integration, removes Unity7 support (which we don't have packaged)
+    # Remove when version > 22.9.0
+    (fetchpatch {
+      url = "https://github.com/AyatanaIndicators/ayatana-indicator-session/commit/8c4df6215a986695edc6c6530f6d6388ea9640d5.patch";
+      hash = "sha256-/9ibFQYtWGXAjgf3lcZvZ3QYJf9KTnlf9dluwzWo+hI=";
+    })
+  ];
+
   postPatch = ''
+    # fetchpatch doesn't handle renames
+    # Remove when version > 22.9.0
+    mv -v src/backend-dbus/{org.ayatana.Desktop,com.lomiri.Shell}.Session.xml
+    mv -v tests/backend-dbus/mock-{unity,lomiri}-session.cc
+    mv -v tests/backend-dbus/mock-{unity,lomiri}-session.h
+
     # Queries systemd user unit dir via pkg_get_variable, can't override prefix
     substituteInPlace data/CMakeLists.txt \
       --replace 'DESTINATION "''${SYSTEMD_USER_DIR}"' 'DESTINATION "${placeholder "out"}/lib/systemd/user"' \
