d12c585e9057b4aa07c29ff602d53a2377f6c763
OPNA2608 <christoph.neidahl@gmail.com>
2023-02-25 23:30:41 +0100

    lomiri-schemas: 0.1.1 -> 0.1.3

--- pkgs/development/libraries/lomiri-schemas/default.nix
+++ pkgs/development/libraries/lomiri-schemas/default.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "lomiri-schemas";
-  version = "0.1.1";
+  version = "0.1.3";
 
   src = fetchFromGitLab {
     owner = "ubports";
     repo = "development/core/${pname}";
     rev = version;
-    hash = "sha256-rDktTTk5SLQ3wJiu0g1WPv6Rh560Rx7c50WuNVlXSew=";
+    hash = "sha256-FrDUFqdD0KW2VG2pTA6LMb6/9PdNtQUlYTEo1vnW6QQ=";
   };
 
   strictDeps = true;
