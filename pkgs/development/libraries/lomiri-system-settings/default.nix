1dcafdbc365d431744376e13d821b3ae6f600345
OPNA2608 <christoph.neidahl@gmail.com>
2023-03-05 17:14:00 +0100

    lomiri-system-settings: Work on tests, improve packaging

--- pkgs/development/libraries/lomiri-system-settings/default.nix
+++ pkgs/development/libraries/lomiri-system-settings/default.nix
@@ -1,9 +1,17 @@
 # TODO
 # - tests
 # - meta
+# - schemas
+#   - com.lomiri.keyboard.maliit = lomiri-keyboard
+#     - or patch into regular maliit-keyboard instead?
+# - keep Ubuntu-inised accountsservice or patch out Ubuntu-specific code?
+# - some of the plugin detection needs to be rewritten to check /run/current-system/sw
+#   i.e. whether battery entry should be displayed:
+#   "visible-if-file-exists": "/etc/dbus-1/system.d/com.lomiri.Repowerd.conf"
 { stdenv
 , lib
 , fetchFromGitLab
+, fetchpatch
 , cmake
 , pkg-config
 , qtbase
@@ -23,8 +31,28 @@
 , icu
 , cmake-extras
 , xvfb-run
+, dbus
+, lomiri-ui-toolkit
+, lomiri-settings-components
+, qtgraphicaleffects
+, python3
+, wrapQtAppsHook
+, qtfeedback
+, qmenumodel
+, qtsystems
+, lomiri-indicator-network
+, libqofono
+, wrapGAppsHook
+, lomiri-schemas
+, ayatana-indicator-datetime
+, content-hub
 }:
 
+let
+  pythonEnv = python3.withPackages (ps: with ps; [
+    python-dbusmock
+  ]);
+in
 stdenv.mkDerivation rec {
   pname = "lomiri-system-settings";
   version = "1.0.1";
@@ -36,8 +64,25 @@ stdenv.mkDerivation rec {
     hash = "sha256-7XJ2mvqcI+tBEpT6tAVJrcEzyDhiY1ttB1X1e24kmd8=";
   };
 
+  patches = [
+    # Fix tests on newer python-dbusmock
+    # https://gitlab.com/ubports/development/core/lomiri-system-settings/-/merge_requests/354
+    (fetchpatch {
+      url = "https://gitlab.com/ubports/development/core/lomiri-system-settings/-/commit/b6c7a807daa85ab8a28b80564a3e29d50946145a.patch";
+      hash = "sha256-CrtisskULD7zFMJMe0Ebusgxso6WPAN3hk8egxElnK8=";
+    })
+    (fetchpatch {
+      url = "https://gitlab.com/ubports/development/core/lomiri-system-settings/-/commit/8698bf41f21456a866baa52849a7fd200470e1c9.patch";
+      hash = "sha256-cEldfwQl2Uuk80Myaf9w4aOmHGDphK20I7GrhuotNrU=";
+    })
+  ];
+
   postPatch = ''
-    substituteInPlace  lib/LomiriSystemSettings/LomiriSystemSettings.pc.in \
+    substituteInPlace CMakeLists.txt \
+      --replace '/qt5/qml' '/qt-${qtbase.version}/qml' \
+      --replace "\''${CMAKE_INSTALL_PREFIX}/\''${LIBDIR}" "\''${CMAKE_INSTALL_LIBDIR}"
+
+    substituteInPlace lib/LomiriSystemSettings/LomiriSystemSettings.pc.in \
       --replace "\''${prefix}/@LIBDIR@" '@CMAKE_INSTALL_FULL_LIBDIR@'
   '';
 
@@ -47,7 +92,10 @@ stdenv.mkDerivation rec {
     cmake
     pkg-config
     gettext
+    glib # glib-compile-schemas
     intltool
+    wrapGAppsHook
+    wrapQtAppsHook
   ];
 
   buildInputs = [
@@ -65,11 +113,65 @@ stdenv.mkDerivation rec {
     qtbase
     qtdeclarative
     upower
+    lomiri-ui-toolkit
+    lomiri-settings-components
+
+    # QML
+    qtfeedback # lomiri-ui-toolkit
+    qmenumodel
+    qtsystems
+    lomiri-indicator-network
+    libqofono
+
+    # Schemas
+    lomiri-schemas
+    ayatana-indicator-datetime
+    content-hub
   ];
 
   nativeCheckInputs = [
+    dbus
+    pythonEnv
     xvfb-run
   ];
 
-  dontWrapQtApps = true;
+  checkInputs = [
+    qtgraphicaleffects # tests-only?
+  ];
+
+  cmakeFlags = [
+    "-DENABLE_TESTS=${lib.boolToString doCheck}"
+  ] ++ lib.optionals doCheck [
+    # https://gitlab.com/ubports/development/core/lomiri-system-settings/-/issues/327
+    "-DMODERN_PYTHON_DBUSMOCK=ON"
+  ];
+
+  postInstall = ''
+    glib-compile-schemas $out/share/glib-2.0/schemas
+  '';
+
+  # TODO Failing with 2 segfaults
+  doCheck = false;
+
+  enableParallelChecking = false;
+
+  checkPhase = ''
+    runHook preCheck
+
+    export QT_PLUGIN_PATH=${lib.getBin qtbase}/lib/qt-${qtbase.version}/plugins
+    export QML2_IMPORT_PATH=${lib.getBin qtdeclarative}/lib/qt-${qtbase.version}/qml:${lib.getBin lomiri-ui-toolkit}/lib/qt-${qtbase.version}/qml:${lib.getBin lomiri-settings-components}/lib/qt-${qtbase.version}/qml:${lib.getBin qtgraphicaleffects}/lib/qt-${qtbase.version}/qml
+
+    dbus-run-session --config-file=${dbus}/share/dbus-1/session.conf -- \
+      make test
+
+    runHook postCheck
+  '';
+
+  dontWrapGApps = true;
+
+  preFixup = ''
+    qtWrapperArgs+=(
+      "''${gappsWrapperArgs[@]}"
+    )
+  '';
 }
