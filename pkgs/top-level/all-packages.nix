c72669021708abe5bb7930e53b86643f86fed59f
OPNA2608 <christoph.neidahl@gmail.com>
2023-03-14 16:13:02 +0100

    morph-browser: init at 1.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -40034,6 +40034,8 @@ with pkgs;
 
   lomiri-ui-extras = libsForQt5.callPackage ../development/libraries/lomiri-ui-extras { };
 
+  morph-browser = libsForQt5.callPackage ../development/libraries/morph-browser { };
+
   # lomiri end
 
   ufiformat = callPackage ../tools/system/ufiformat { };
