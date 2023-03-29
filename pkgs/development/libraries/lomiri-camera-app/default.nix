5119ebe5d962ea65630b1f81d8a84c8517affb2e
OPNA2608 <christoph.neidahl@gmail.com>
2023-03-29 02:51:00 +0200

    lomiri-camera-app: Fix splash icon, add rest of gstreamer plugins
    bad -> base, other ones just in case (revisit in the future?)

--- pkgs/development/libraries/lomiri-camera-app/default.nix
+++ pkgs/development/libraries/lomiri-camera-app/default.nix
@@ -43,7 +43,8 @@ stdenv.mkDerivation rec {
     substituteInPlace CMakeLists.txt \
       --replace "\''${CMAKE_INSTALL_DATADIR}/\''${CAMERA_APP}" "\''${CMAKE_INSTALL_FULL_DATADIR}/\''${CAMERA_APP}" \
       --replace "\''${CMAKE_INSTALL_PREFIX}/\''${CAMERA_APP_DIR}" "\''${CAMERA_APP_DIR}" \
-      --replace "\''${CMAKE_INSTALL_LIBDIR}/\''${CAMERA_APP}" "\''${CMAKE_INSTALL_FULL_LIBDIR}/\''${CAMERA_APP}"
+      --replace "\''${CMAKE_INSTALL_LIBDIR}/\''${CAMERA_APP}" "\''${CMAKE_INSTALL_FULL_LIBDIR}/\''${CAMERA_APP}" \
+      --replace "\''${CAMERA_APP_DIR}/lomiri" "\''${CAMERA_APP_DIR}/assets/lomiri"
   '' + lib.optionalString (!doCheck) ''
     sed -i -e '/add_subdirectory(tests)/d' CMakeLists.txt
   '';
@@ -76,7 +77,10 @@ stdenv.mkDerivation rec {
   ] ++ (with gst_all_1; [
     # cannot create camera service, the 'camerabin' plugin is missing for GStreamer
     gstreamer
+    gst-plugins-base
+    gst-plugins-good
     gst-plugins-bad
+    gst-plugins-ugly
   ]);
 
   cmakeFlags = [
