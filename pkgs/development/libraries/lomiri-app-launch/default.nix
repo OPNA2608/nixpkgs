b50a5d0b63231d6e78d75ea3df847c2e923d685b
OPNA2608 <christoph.neidahl@gmail.com>
2023-05-26 23:09:57 +0200

    lomiri-app-launch: Add meta, tests

--- pkgs/development/libraries/lomiri-app-launch/default.nix
+++ pkgs/development/libraries/lomiri-app-launch/default.nix
@@ -1,31 +1,35 @@
 # TODO
 # - AA_EXEC_PATH apparmor path for by-hand apparmor usage (needed?)
-# - docs
-# - test
 # - meta
 { stdenv
 , lib
 , fetchFromGitLab
+#, apparmor-bin-utils
 , cmake
 , cmake-extras
-, pkg-config
-, gobject-introspection
-, apparmor-bin-utils
-, glib
-, json-glib
-, ubports-click
-, zeitgeist
+, curl
 , dbus
 , dbus-test-runner
-, lttng-ust
-, curl
-, lomiri-api
+#, glib
+, gobject-introspection
 , gtest
+, json-glib
 , libxkbcommon
+, lomiri-api
+, lttng-ust
+, pkg-config
 , properties-cpp
+, python3
 , systemd
+, ubports-click
+, zeitgeist
 }:
 
+let
+  pythonEnv = python3.withPackages (ps: with ps; [
+    python-dbusmock
+  ]);
+in
 stdenv.mkDerivation rec {
   pname = "lomiri-app-launch";
   version = "0.1.6";
@@ -38,9 +42,14 @@ stdenv.mkDerivation rec {
   };
 
   postPatch = ''
+    patchShebangs tests/{desktop-hook-test.sh.in,repeat-until-pass.sh}
+
     # used pkg_get_variable, cannot replace prefix
     substituteInPlace data/CMakeLists.txt \
       --replace 'DESTINATION "''${SYSTEMD_USER_UNIT_DIR}"' 'DESTINATION "${placeholder "out"}/lib/systemd/user"'
+
+    substituteInPlace tests/jobs-systemd.cpp \
+      --replace '^(/usr)?' '^(/nix/store/\\w+-bash-.+)?'
   '';
 
   strictDeps = true;
@@ -54,21 +63,40 @@ stdenv.mkDerivation rec {
     cmake-extras
     curl
     dbus
-    dbus-test-runner
     gobject-introspection
-    gtest
     json-glib
+    libxkbcommon
     lomiri-api
-    ubports-click
     lttng-ust
     properties-cpp
-    libxkbcommon
-    zeitgeist
     systemd
+    ubports-click
+    zeitgeist
+  ];
+
+  nativeCheckInputs = [
+    dbus
+    pythonEnv
+  ];
+
+  checkInputs = [
+    dbus-test-runner
+    gtest
   ];
 
   cmakeFlags = [
     "-DENABLE_MIRCLIENT=OFF"
     "-DLOMIRI_APP_LAUNCH_ARCH=${stdenv.hostPlatform.config}"
+    "-DENABLE_TESTS=${lib.boolToString doCheck}"
   ];
+
+  doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
+
+  meta = with lib; {
+    description = "System and associated utilities to launch applications in a standard and confined way";
+    homepage = "https://gitlab.com/ubports/development/core/lomiri-app-launch";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ OPNA2608 ];
+    platforms = platforms.linux;
+  };
 }
