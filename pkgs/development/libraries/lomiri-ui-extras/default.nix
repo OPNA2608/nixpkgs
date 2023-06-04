cea6faf606f1f408ecc5ff7490d61ff01f0c7e26
OPNA2608 <christoph.neidahl@gmail.com>
2023-06-05 00:57:39 +0200

    lomiri-ui-extras: Add meta, give up on tests
    Failing Tests: Prepare To Cry Edition

--- pkgs/development/libraries/lomiri-ui-extras/default.nix
+++ pkgs/development/libraries/lomiri-ui-extras/default.nix
@@ -1,6 +1,3 @@
-# TODO
-# - tests
-# - meta
 { stdenv
 , lib
 , fetchFromGitLab
@@ -8,10 +5,12 @@
 , cmake-extras
 , cups
 , exiv2
+, lomiri-ui-toolkit
 , pam
 , pkg-config
 , qtbase
 , qtdeclarative
+, xvfb-run
 }:
 
 stdenv.mkDerivation rec {
@@ -32,6 +31,11 @@ stdenv.mkDerivation rec {
   postPatch = ''
     substituteInPlace modules/Lomiri/Components/Extras{,/{plugin,PamAuthentication}}/CMakeLists.txt \
       --replace "\''${CMAKE_INSTALL_LIBDIR}/qt5/qml" '${qtbase.qtQmlPrefix}'
+
+    # Don't disregard PATH when searching for qmltestrunner
+    sed -i \
+      -e '/NO_DEFAULT_PATH/d' \
+      tests/qml/CMakeLists.txt
   '';
 
   strictDeps = true;
@@ -50,12 +54,47 @@ stdenv.mkDerivation rec {
     qtdeclarative
   ];
 
+  nativeCheckInputs = [
+    qtdeclarative # qmltestrunner
+    xvfb-run
+  ];
+
+  checkInputs = [
+    lomiri-ui-toolkit
+  ];
+
   dontWrapQtApps = true;
 
   cmakeFlags = [
     "-DENABLE_TESTS=${lib.boolToString doCheck}"
   ];
 
-  # TODO
+  # QML tests work when run under nix-shell --pure / nix develop --ignore-environment, 1/3 consistently segfaults under nix-build
+  # I do not understand why, and I can not debug why. I want to cry.
   doCheck = false;
+
+  # Parallelism breaks the QML tests, seemingly ripping away the xvfb-run-launched server under their feet
+  enableParallelChecking = false;
+
+  preCheck = let
+    qtToEnvvar = drvs: prefix: lib.strings.concatMapStringsSep ":" (drv: "${lib.getBin drv}/${prefix}") drvs;
+  in ''
+    export QT_PLUGIN_PATH=${qtToEnvvar [ qtbase] qtbase.qtPluginPrefix}
+    export QML2_IMPORT_PATH=${qtToEnvvar ([ qtdeclarative lomiri-ui-toolkit ] ++ lomiri-ui-toolkit.propagatedBuildInputs) qtbase.qtQmlPrefix}
+    export HOME=$PWD
+    export XDG_RUNTIME_DIR=$PWD
+  '';
+
+  meta = with lib; {
+    description = "Lomiri UI Extra Components";
+    longDescription = ''
+      A collection of UI components that for various reasons can't be included in
+      the main Lomiri UI toolkit - mostly because of the level of quality, lack of
+      documentation and/or lack of automated tests.
+    '';
+    homepage = "https://gitlab.com/ubports/development/core/lomiri-ui-extras";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ OPNA2608 ];
+    platforms = platforms.linux;
+  };
 }
