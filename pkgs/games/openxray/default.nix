6ff301da29504d692c4cd42bc28acfe3c00a1189
OPNA2608 <christoph.neidahl@gmail.com>
2023-07-22 01:39:56 +0200

    openxray: 1747-january-2023-rc3 -> unstable-2023-07-22

--- pkgs/games/openxray/default.nix
+++ pkgs/games/openxray/default.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, fetchpatch
 , cmake
 , glew
 , freeimage
@@ -18,25 +17,15 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "openxray";
-  version = "1747-january-2023-rc3";
+  version = "unstable-2023-07-20";
 
   src = fetchFromGitHub {
     owner = "OpenXRay";
     repo = "xray-16";
-    rev = finalAttrs.version;
+    rev = "80fa92baa15ffc3ac712b54ea899a9cf90877986";
     fetchSubmodules = true;
-    hash = "sha256-ip9CruOWk5T/dBDMcn9aOHbYaaZ+7VJw9U5GayiE7AE=";
+    hash = "sha256-nPj5zrEkO/R8y9Vh1hSb9XnB3lJsSfYqhgVPr/qNtmE=";
   };
-
-  patches = [
-    # Fix OpenGL shader lookup & linking
-    # Remove when in a tag
-    (fetchpatch {
-      name = "0001-openxray-Fix-resource-linking-again.patch";
-      url = "https://github.com/OpenXRay/xray-16/commit/88d2302b490ff62344c5ab4f8ba77260402cf4f4.patch";
-      hash = "sha256-JvOwnqTThYB41ndpGrW1jVUp7rcysCTHNCEyq1fTlrY=";
-    })
-  ];
 
   strictDeps = true;
 
@@ -65,11 +54,11 @@ stdenv.mkDerivation (finalAttrs: {
   postInstall = ''
     # needed because of SDL_LoadObject library loading code
     wrapProgram $out/bin/xr_3da \
-      --prefix LD_LIBRARY_PATH : $out/lib
+      --prefix ${if stdenv.hostPlatform.isDarwin then "DYLD_LIBRARY_PATH" else "LD_LIBRARY_PATH"} : $out/lib
   '';
 
   meta = with lib; {
-    mainProgram = "xray-16";
+    mainProgram = "xr_3da";
     description = "Improved version of the X-Ray Engine, the game engine used in the world-famous S.T.A.L.K.E.R. game series by GSC Game World";
     homepage = "https://github.com/OpenXRay/xray-16/";
     license = licenses.unfree // {
