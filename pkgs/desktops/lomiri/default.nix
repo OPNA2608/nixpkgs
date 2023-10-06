f58effa1d5a614dcfae8a9000b6d9bbc659bf6ce
OPNA2608 <christoph.neidahl@gmail.com>
2023-10-06 15:09:53 +0200

    lomiri.lomiri-url-dispatcher: init at 0.1.3

--- pkgs/desktops/lomiri/default.nix
+++ pkgs/desktops/lomiri/default.nix
@@ -32,6 +32,7 @@ let
     biometryd = callPackage ./services/biometryd { };
     hfd-service = callPackage ./services/hfd-service { };
     lomiri-download-manager = callPackage ./services/lomiri-download-manager { };
+    lomiri-url-dispatcher = callPackage ./services/lomiri-url-dispatcher { };
   };
 in
   lib.makeScope libsForQt5.newScope packages
