a2d5ce2be3fb484550974eeb4d0cbf0872a964cb
OPNA2608 <opna2608@protonmail.com>
2024-01-14 23:24:20 +0100

    {libsForQt5,qt6Packages}.qmltermwidget: unstable-2022-01-09 -> 0-unstable-2022-01-09

--- pkgs/development/libraries/qmltermwidget/default.nix
+++ pkgs/development/libraries/qmltermwidget/default.nix
@@ -10,7 +10,7 @@
 
 stdenv.mkDerivation {
   pname = "qmltermwidget";
-  version = "unstable-2022-01-09";
+  version = "0-unstable-2022-01-09";
 
   src = fetchFromGitHub {
     owner = "Swordfish90";
