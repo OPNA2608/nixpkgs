2ebaecacf5546995d489b7e12e4f9e5958274679
OPNA2608 <opna2608@protonmail.com>
2025-12-10 08:13:18 +0100

    linux/common-config: Add settings for 64-bit POWER

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -990,6 +990,9 @@ let
       XEN_PVHVM = option yes;
       XEN_SAVE_RESTORE = option yes;
 
+      # Disabled by default on POWER
+      VIRTIO_MENU = yes;
+
       # Enable device detection on virtio-mmio hypervisors
       VIRTIO_MMIO_CMDLINE_DEVICES = yes;
 
@@ -1437,6 +1440,10 @@ let
         # Enable coreboot firmware drivers.
         # While these are called CONFIG_GOOGLE_*, they apply to coreboot systems in general.
         GOOGLE_FIRMWARE = yes;
+
+        # Disabled by default on POWER
+        ATA_BMDMA = yes;
+        ATA_SFF = yes;
       }
       //
         lib.optionalAttrs
@@ -1552,6 +1559,11 @@ let
 
         # Enable Intel Turbo Boost Max 3.0
         INTEL_TURBO_MAX_3 = yes;
+      }
+      // lib.optionalAttrs (stdenv.hostPlatform.isPower64) {
+        # avoid driver/FS trouble arising from unusual page size
+        PPC_64K_PAGES = no;
+        PPC_4K_PAGES = yes;
       };
 
     accel = {
