f6cf25102c19238d4d4287803106b9ab8c47b44f
OPNA2608 <opna2608@protonmail.com>
2026-05-19 13:24:54 +0200

    maliit-framework: Misc fixes & improvements
    - Drop unused fetchpatch input
    - Make documentation optional
    - Fix log spam during documentation generation
    - Add optional examples building
    - Fix QMake mkspecs install location
    - Fix doubled prefixes in mkspecs files
    - Split everything into different outputs
    - Make test building depend on doCheck
    - Actually run the build tests

--- pkgs/applications/misc/maliit-framework/default.nix
+++ pkgs/applications/misc/maliit-framework/default.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
 
   qtbase,
 
@@ -22,13 +21,19 @@
 
   cmake,
   doxygen,
+  fontconfig,
+  graphviz,
   pkg-config,
   wayland-protocols,
   wayland-scanner,
   wrapQtAppsHook,
+  writableTmpDirAsHomeHook,
+
+  enableDocumentation ? true,
+  enableExamples ? true,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "maliit-framework";
   version = "2.3.0-unstable-2024-06-24";
 
@@ -38,6 +43,33 @@ stdenv.mkDerivation {
     rev = "ba6f7eda338a913f2c339eada3f0382e04f7dd67";
     hash = "sha256-iwWLnstQMG8F6uE5rKF6t2X43sXQuR/rIho2RN/D9jE=";
   };
+
+  outputs = [
+    "out"
+    "dev"
+  ]
+  ++ lib.optionals enableDocumentation [
+    "doc"
+  ]
+  ++ lib.optionals enableExamples [
+    "examples"
+  ];
+
+  postPatch =
+    # Fix doubled prefixes
+    ''
+      substituteInPlace common/maliit-framework.prf.in \
+        --replace-fail '@CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@' '@CMAKE_INSTALL_FULL_INCLUDEDIR@'
+
+      substituteInPlace src/maliit-plugins.prf.in \
+        --replace-fail '@CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_LIBDIR@' '@CMAKE_INSTALL_FULL_LIBDIR@' \
+        --replace-fail '@CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@' '@CMAKE_INSTALL_FULL_INCLUDEDIR@'
+    ''
+    # Use maliit-server from this build
+    + ''
+      substituteInPlace examples/apps/plainqt/mainwindow.cpp \
+        --replace-fail 'serverName("maliit-server")' "serverName(\"$out/bin/maliit-server\")"
+    '';
 
   buildInputs = [
     at-spi2-atk
@@ -57,16 +89,35 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [
     cmake
-    doxygen
     pkg-config
     wayland-protocols
     wayland-scanner
     wrapQtAppsHook
+  ]
+  ++ lib.optionals enableDocumentation [
+    doxygen
+    graphviz
+    writableTmpDirAsHomeHook
   ];
 
   cmakeFlags = [
-    "-DQT5_PLUGINS_INSTALL_DIR=${placeholder "out"}/${qtbase.qtPluginPrefix}"
+    (lib.strings.cmakeBool "enable-docs" enableDocumentation)
+    (lib.strings.cmakeBool "enable-examples" enableExamples)
+    (lib.strings.cmakeBool "enable-tests" finalAttrs.finalPackage.doCheck)
+    (lib.strings.cmakeFeature "QT5_MKSPECS_INSTALL_DIR" "${placeholder "out"}/mkspecs")
+    (lib.strings.cmakeFeature "QT5_PLUGINS_INSTALL_DIR" "${placeholder "out"}/${qtbase.qtPluginPrefix}")
   ];
+
+  doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
+
+  postInstall = lib.optionalString enableExamples ''
+    moveToOutput "bin/maliit-exampleapp-*" "$examples"
+  '';
+
+  env = {
+    FONTCONFIG_FILE = lib.optionalString enableDocumentation "${fontconfig.out}/etc/fonts/fonts.conf";
+    QT_PLUGIN_PATH = lib.optionalString finalAttrs.finalPackage.doCheck "${lib.getBin qtbase}/${qtbase.qtPluginPrefix}";
+  };
 
   meta = {
     description = "Core libraries of Maliit and server";
@@ -75,4 +126,4 @@ stdenv.mkDerivation {
     license = lib.licenses.lgpl21Plus;
     maintainers = [ ];
   };
-}
+})
