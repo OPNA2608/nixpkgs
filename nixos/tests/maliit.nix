9ad6fe9bd042519970f949c72e61bce84d31aa31
OPNA2608 <opna2608@protonmail.com>
2026-05-20 23:52:55 +0200

    WIP nixosTests.maliit.lomiri-lomiri-keyboard: init

--- nixos/tests/maliit.nix
+++ nixos/tests/maliit.nix
@@ -1,62 +1,81 @@
 let
   makeTest = import ./make-test-python.nix;
-in
-{
-  x11 = makeTest (
-    { lib, ... }:
-    {
-      name = "maliit-x11";
-      meta.maintainers = [ lib.maintainers.OPNA2608 ];
 
-      nodes.machine =
-        { config, pkgs, ... }:
-        {
-          imports = [ ./common/x11.nix ];
+  wrappedMaliit =
+    pkgs: packagePath: binaryName:
+    pkgs.maliit-framework.passthru.wrapServerWithPlugin {
+      pluginPackage = builtins.foldl' (pkgsSet: subpath: pkgsSet.${subpath}) pkgs packagePath;
+      inherit binaryName;
+    };
 
-          services.xserver.enable = true;
+  pkgsPathToString =
+    pkgpath: if (builtins.isList pkgpath) then (builtins.concatStringsSep "-" pkgpath) else pkgpath;
 
-          environment = {
-            systemPackages =
-              [
+  makeMaliitTest =
+    maliitKeyboardPackagePath:
+    makeTest (
+      { lib, ... }:
+      {
+        name = "maliit-server-wrapper-${pkgsPathToString maliitKeyboardPackagePath}";
+        meta.maintainers = [ lib.maintainers.OPNA2608 ];
+
+        nodes.machine =
+          { config, pkgs, ... }:
+          {
+            imports = [ ./common/x11.nix ];
+
+            services.xserver.enable = true;
+
+            environment = {
+              systemPackages = [
                 # maliit-server wrapped with a keyboard plugin
-                (pkgs.maliit-framework.passthru.wrapServerWithPlugin {
-                  # maliit-keyboard does have its own binary, so this wrapper isn't necessary for it, but we don't have
-                  # any other Maliit plugins packaged to test this with.
-                  pluginPackage = pkgs.maliit-keyboard;
-                  binaryName = "maliit-keyboard";
-                })
+                (wrappedMaliit pkgs maliitKeyboardPackagePath (pkgsPathToString maliitKeyboardPackagePath))
 
                 # example app plays nicely with IM
                 pkgs.maliit-framework.examples
               ];
-            variables = {
-              GTK_IM_MODULE = "Maliit";
-              QT_IM_MODULE = "Maliit";
+              variables = {
+                GTK_IM_MODULE = "Maliit";
+                QT_IM_MODULE = "Maliit";
+              };
             };
           };
-        };
 
-      enableOCR = true;
+        enableOCR = true;
 
-      testScript = ''
-        machine.wait_for_x()
+        testScript = ''
+          machine.wait_for_x()
 
-        machine.succeed("env DISPLAY=:0 xterm >&2 &")
-        machine.sleep(5)
+          machine.succeed("env DISPLAY=:0 xterm >&2 &")
+          machine.sleep(5)
 
-        machine.send_chars("maliit-keyboard &\n")
-        machine.sleep(5)
+          machine.send_chars("${pkgsPathToString maliitKeyboardPackagePath} &\n")
+          machine.sleep(5)
 
-        machine.send_chars("maliit-exampleapp-plainqt\n")
-        machine.sleep(5)
-        machine.screenshot("maliit-server-wrapper_initial")
+          machine.send_chars("maliit-exampleapp-plainqt\n")
+          machine.sleep(5)
+          machine.screenshot("maliit-server-wrapper_initial")
 
-        machine.send_key("tab")
-        machine.sleep(5)
+          machine.send_key("tab")
+          machine.sleep(5)
 
-        machine.wait_for_text("English")
-        machine.screenshot("maliit-server-wrapper_works")
-      '';
-    }
-  );
-}
+          machine.wait_for_text("English")
+          machine.screenshot("maliit-server-wrapper_works")
+        '';
+      }
+    );
+in
+builtins.listToAttrs (
+  builtins.map
+    (pkgpath: {
+      name = pkgsPathToString pkgpath;
+      value = makeMaliitTest pkgpath;
+    })
+    [
+      "maliit-keyboard"
+      ([
+        "lomiri"
+        "lomiri-keyboard"
+      ])
+    ]
+)
